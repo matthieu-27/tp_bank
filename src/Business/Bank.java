59e23935e4405b0bc3325a502498b2522b7d9e2c
matthieu-27 <matthieu27@pm.me>
2025-12-19 13:09:22 +0100

    fix: ajout du dao.close()

--- src/Business/Bank.java
+++ src/Business/Bank.java
@@ -5,15 +5,17 @@ import Models.Account;
 
 public class Bank {
 	
-	public static void deposit(double amount, Account account) throws InvalidAmountException {
+	public static void deposit(double amount, Account account) throws Exception {
+		AccountDao accountDao = new AccountDao();
         if(amount <= 0 ) {
             throw new InvalidAmountException("Amount cannot be zero or negative");
-        }
+        } 
         try {
-            var accountDao = new AccountDao();
             accountDao.update(account);
         } catch (Exception e) {
             IO.println(e.getMessage());
+        } finally {
+        	accountDao.close();
         }
 	}
 
